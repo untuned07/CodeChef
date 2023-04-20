693c1cba57a836136ec2ab2ed0613d11dbe426d4
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2023-04-20 20:52:36 +0530

    Add files via upload

---
+++ WatchingMovieAt2x.cpp
@@ -0,0 +1,12 @@
+// Codechef Problem: https://www.codechef.com/problems/MOVIE2X
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin >>x >>y;
+	cout << x - (y / 2) <<endl;
+	return 0;
+}
